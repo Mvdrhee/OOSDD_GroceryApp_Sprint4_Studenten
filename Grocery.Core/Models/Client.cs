7749de0532d994b23b98a7af8564a2eb8e7a0cb0
Michael <s1197491@student.windesheim.nl>
2025-10-02 16:42:24 +0200

    Role enum uitgewerkt

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -5,10 +5,16 @@ namespace Grocery.Core.Models
     {
         public string EmailAddress { get; set; }
         public string Password { get; set; }
+        public Role Metb { get; set; }
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
-            EmailAddress=emailAddress;
-            Password=password;
+            EmailAddress = emailAddress;
+            Password = password;
+            Metb = Role.None;
+        }
+        public Client(int id, string name, string emailAddress, string password, Role role) : this(id, name, emailAddress, password)
+        {
+            Metb = role;
         }
     }
 }
